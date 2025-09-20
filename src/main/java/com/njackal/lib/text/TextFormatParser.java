5258174fca3c4a5d6bc592c92382d1190ae22d16
VanJackal <v@njackal.com>
2025-09-20 16:37:23 -0500

    rename formatting

--- src/main/java/com/njackal/lib/text/TextFormatParser.java
+++ src/main/java/com/njackal/lib/text/TextFormatParser.java
@@ -1,8 +1,6 @@
 package com.njackal.lib.text;
 
-import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,15 +8,14 @@ import java.util.regex.Pattern;
 public class TextFormatParser {
     private final Pattern pattern;
     public TextFormatParser() {
-        pattern = Pattern.compile("&(\\S)");
+        pattern = Pattern.compile("(?<!\\\\)(&)([0-9rlmnok])", Pattern.CASE_INSENSITIVE);
     }
 
     public Text format(String text) {
         Matcher matcher = pattern.matcher(text);
-        System.out.println(text);
-        Text out;
-        String newText = text.replace("&", "§");
-        out = Text.literal(newText);
-        return out;
+        StringBuilder formattedText = new StringBuilder(text);
+        matcher.results().forEach(result ->
+                formattedText.replace(result.start(1),result.end(1), "§"));
+        return Text.literal(formattedText.toString());
     }
 }
